fda5c45d291849d90b07a0d5119c7f8a043cf1ed
Andy Joiner <andy.joiner@tekkies.co.uk>
2017-03-09 21:12:45 +0000

    Create simulation_test_wake_command_prompt()
    Simulation to wait for a message from the browser, then send the repeating > prompt
    
    #risk2 #pi-rev11-web-console #evolve

--- pi_rev11_web_console/simulatedcomms.py
+++ pi_rev11_web_console/simulatedcomms.py
@@ -2,14 +2,16 @@ import threading
 import time
 
 import datetime
-
-
+import queue
 
 
 class SimulatedComms:
 
+
+
     def __init__(self, router):
         self.router = router
+        self.receive_queue = queue.Queue()
         router.register_comms(self)
         thread = threading.Thread(target=self.simulator_main, args=(0, 0))
         thread.daemon = True
@@ -22,18 +24,32 @@ class SimulatedComms:
 
     def tx(self, data):
         self.router.on_tx(data)
-        self.simulate_command(data)
+        self.receive_queue.put(data)
+        #self.simulate_command(data)
 
     def on_rx(self, data):
         self.router.on_rx(data)
 
     def simulator_main(self, a,b):
+        self.simulation_test_wake_command_prompt()
+
+    def simulate_command_prompt(self):
         loop = 0
         while True:
             time.sleep(1)
-            if(loop % 10 == 0):
-                self.on_rx("simulated message \"%s\"" % datetime.datetime.now().time())
+            if (loop % 10 == 0):
+                self.on_rx("Fake simulated message \"%s\"" % datetime.datetime.now().time())
             else:
                 self.on_rx("")
                 self.on_rx(">")
             loop = loop + 1
+
+
+    def simulation_test_wake_command_prompt(self):
+        loop = 0
+        while True:
+            if not self.receive_queue.empty():
+                rx = self.receive_queue.get_nowait()
+                self.simulate_command_prompt()
+            time.sleep(0.1)
+
